ca31f7072871d6c08c7b1da9c509889abcadd188
melmasri <elmasri.m@gmail.com>
2024-05-30 12:26:18 +0100

    added  config to get a batch

--- train.py
+++ train.py
@@ -31,23 +31,27 @@ def wandb_init(model):
         wandb.watch(model)
         
 
-def get_batch(split):
+def get_batch(split, config=None):
+    block_size = BLOCK_SIZE
+    batch_size = BATCH_SIZE
+    if config:
+        block_size = config.get('block_size', BLOCK_SIZE)
+        batch_size = config.get('batch_size', BATCH_SIZE)
     # We recreate np.memmap every batch to avoid a memory leak, as per
     # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
     if split == 'train':
         data = np.memmap(os.path.join(INPUT_DATA_FOLDER, 'train.bin'), dtype=np.uint16, mode='r')
     else:
         data = np.memmap(os.path.join(INPUT_DATA_FOLDER, 'val.bin'), dtype=np.uint16, mode='r')
-    ix = torch.randint(len(data) - BLOCK_SIZE, (BATCH_SIZE,))
-    x = torch.stack([torch.from_numpy((data[i:i+BLOCK_SIZE]).astype(np.int64)) for i in ix])
-    y = torch.stack([torch.from_numpy((data[i+1:i+1+BLOCK_SIZE]).astype(np.int64)) for i in ix])
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
     if DEVICE_TYPE == 'cuda':
         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
         x, y = x.pin_memory().to(DEVICE, non_blocking=True), y.pin_memory().to(DEVICE, non_blocking=True)
     else:
         x, y = x.to(DEVICE), y.to(DEVICE)
     return x, y
-
 
 
 def train(model, optimizer, num_epochs):
