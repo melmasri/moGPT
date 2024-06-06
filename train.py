9edf0d86eec9ac8bad861f48ddd9ae826ad8a808
melmasri <elmasri.m@gmail.com>
2024-06-06 11:42:37 +0100

    Added W&B for the model

--- train.py
+++ train.py
@@ -15,9 +15,13 @@ import torch
 from constants import DATASET, INPUT_DATA_FOLDER, BATCH_SIZE, BLOCK_SIZE, DEVICE_TYPE, DEVICE, VOCAB_SIZE
 
 
-def wandb_init(model):
-    if WANDB_LOG:
-        wandb.init(project=WANDB_PROJECT, name=WANDB_RUN_NAME, entity='wandb', reinit=True)
+def wandb_init(model, run_name=None):
+    if WANDB_LOG and WANDB_KEY:
+        print("Logging to wandb with key")
+        if run_name is None:
+            run_name = WANDB_RUN_NAME 
+        wandb.login(key=WANDB_KEY)
+        wandb.init(project=WANDB_PROJECT, name=run_name, entity='elmasri-m', reinit=True)
         wandb.config.update({
             'DATASET': DATASET,
             'BATCH_SIZE': BATCH_SIZE,
@@ -26,9 +30,6 @@ def wandb_init(model):
             'DEVICE_TYPE': DEVICE_TYPE,
             'DEVICE': DEVICE,
         })
-        if WANDB_KEY:
-            os.environ["WANDB_API_KEY"] = WANDB_KEY
-            wandb.login(key=WANDB_KEY)
         wandb.watch(model)
         
 
@@ -54,15 +55,37 @@ def get_batch(split, config=None):
         x, y = x.to(DEVICE), y.to(DEVICE)
     return x, y
 
+@torch.no_grad()
+def evaluate(model):
+    model.eval()
+    eval_iter = 100
+    metrics = {}
+    config = model.get_config()
+    mode = ['train', 'eval']
+    for split in mode:
+        total_loss = 0.
+        for i in range(eval_iter):
+            data, targets = get_batch(split, config)
+            logits = model(data)
+            B, T, C = logits.shape
+            targets = targets.view(B*T)
+            logits = logits.view(B*T, C)
+            total_loss += F.cross_entropy(logits, targets).item()
+        metrics[split] = total_loss / eval_iter
+    return metrics
 
-def train(model, optimizer, num_epochs):
-    wandb_init(model)
+def train(model, optimizer, num_epochs, run_name=None):
+    wandb_init(model, run_name=run_name)
+    eval_interval = 100
+    iterations = 1000
+    step = 0
     for epoch in range(num_epochs):
         model.train()
         total_loss = 0.
         start_time = time.time()
-        for batch, i in enumerate(range(0, 1000, BATCH_SIZE)):
-            data, targets = get_batch('train')
+        for batch, i in enumerate(range(0, iterations, BATCH_SIZE)):
+            step += 1
+            data, targets = get_batch('train',  model.get_config())
             optimizer.zero_grad()
             logits = model(data)
             B, T, C = logits.shape
@@ -71,8 +94,14 @@ def train(model, optimizer, num_epochs):
             loss = F.cross_entropy(logits, targets)
             loss.backward()
             optimizer.step()
-            if WANDB_LOG:
-                wandb.log({'loss': loss.item()})
+            # logging evaluations
+            if batch % eval_interval == 0:
+                metrics = evaluate(model)
+                if WANDB_LOG:
+                    wandb.log({'train/loss': metrics['train'],
+                               'eval/loss': metrics['eval'],
+                               'step': step,
+                               })
             total_loss += loss.item()
             if batch % 10 == 0 and batch > 0:
                 cur_loss = total_loss / 10
